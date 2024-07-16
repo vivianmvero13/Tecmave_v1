3163cac3ee7959cae4c5272ca0bc1e7c3a7d7815
vivianmvero13 <165861899+vivianmvero13@users.noreply.github.com>
2024-07-16 10:30:32 -0600

    Initial commit

---
+++ src/main/java/com/tecmave/TecmaveApplication.java
@@ -0,0 +1,13 @@
+package com.tecmave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecmaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecmaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecmave/TecmaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecmave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecmaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
