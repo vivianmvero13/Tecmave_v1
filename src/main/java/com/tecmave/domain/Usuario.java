f835da4b5dd81c5e3ae6a5b69c8ee8329eb481ea
vivianmvero13 <165861899+vivianmvero13@users.noreply.github.com>
2024-09-02 12:58:57 -0600

    usuarios fix

--- src/main/java/com/tecmave/domain/Usuario.java
+++ src/main/java/com/tecmave/domain/Usuario.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tecmave.domain;
 
 import jakarta.persistence.Column;
@@ -16,22 +12,28 @@ import java.io.Serializable;
 import java.util.List;
 import lombok.Data;
 
-
 @Data
 @Entity
-@Table(name="usuario")
-public class Usuario implements Serializable{
+@Table (name="usuario")
+public class Usuario implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id_usuario")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
     private Long idUsuario;
-    private String username, password, nombre, apellidos, correo;
+    private String username;
+    private String password;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String telefono;
     private String rutaImagen;
     private boolean activo;
-    
+
     @OneToMany
-    @JoinColumn(name="id_usuario",updatable = false)
-    private List<Rol> roles;
+    @JoinColumn(name = "id_usuario")
+    List<Rol> roles;
+    
 }
+ 
